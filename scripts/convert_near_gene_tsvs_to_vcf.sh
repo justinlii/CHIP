e4f74df126e92991737f5ef438821fd405f10806
Justin Li <74877182+justinlii@users.noreply.github.com>
2026-06-19 21:58:13 -0400

    Create convert_near_gene_tsvs_to_vcf.sh script
    Add script to convert TSV files to VCF format for variants near genes.

---
+++ scripts/convert_near_gene_tsvs_to_vcf.sh
@@ -0,0 +1,9 @@
+
+# The goal is to run fast_borzoi_sed.sh for all variants in 
+# /lab-share/CHIP-Strober-e2/Public/ben/rare_variant_s2e/preprocess_rare_variants/gtex.eur.pass.rare_maf_lt_0.0025.snvs.chr*.rare_variant_carriers_near_genes.tsv.gz
+
+#####################
+# Input data
+#####################
+
+# Directory containing source files (which have additional columns including RARE_ALLELE, MAF, RARE_AC, AN, CARRIERS, and gene_ids
